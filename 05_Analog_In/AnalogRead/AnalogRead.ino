c0ba8be2d39c8c1b8c7758586e1d068cf6432a2b
LanWting <15671677156@163.com>
2018-05-03 15:49:09 +0800

    add some esp32 arduino project files.

---
+++ 05_Analog_In/AnalogRead/AnalogRead.ino
@@ -0,0 +1,19 @@
+#define RLED 16
+#define InPin 33
+
+int x ;
+
+void setup() {
+   Serial.begin(115200);
+   pinMode(RLED, OUTPUT);
+}
+
+void loop()
+{
+   digitalWrite(RLED, LOW);
+   delay(100);
+   x = analogRead( InPin );
+   Serial.println( x );
+   digitalWrite(RLED, HIGH);
+   delay(100);
+}
